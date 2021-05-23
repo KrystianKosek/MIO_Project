7767f9c39b3c04324e0ffbefad226f6a28785db7
Krystian Kosek <krystian.kosek17@gmail.com>
2021-05-23 11:12:07 +0200

    Small refactor, added simple model showing acc

--- train.py
+++ train.py
@@ -1,15 +1,15 @@
 import os
-
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras import backend as K
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.core import Activation, Dropout, Flatten, Dense
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
 from tensorflow import keras
+from tensorflow.python.tpu import datasets
 
-# [3]
+# data
 main_dir = "./chest_xray/"
 train_data_dir = main_dir + "train/"
 validation_data_dir = main_dir + "val/"
@@ -17,12 +17,9 @@ test_data_dir = main_dir + "test/"
 
 nb_train_samples = 5216
 nb_validation_samples = 16
-epochs = 1
+epochs = 4
 batch_size = 16
 
-print("Working Directory Contents:", os.listdir(main_dir))
-
-# [4]
 train_n = train_data_dir + 'NORMAL/'
 train_p = train_data_dir + 'PNEUMONIA/'
 
@@ -30,9 +27,7 @@ print("length of cases in training set:", len(os.listdir(train_p)) + len(os.list
 print("length of pneumonia cases in training set:", len(os.listdir(train_p)))
 print("length of normal cases in training set:", len(os.listdir(train_n)))
 
-# [5]
-
-# [6]
+# images size
 img_height, img_width = 180, 180
 
 if K.image_data_format() == 'channels_first':
@@ -40,7 +35,6 @@ if K.image_data_format() == 'channels_first':
 else:
     input_shape = (img_width, img_height, 3)
 
-# [7]
 # Performing Image Augmentation to have more data samples
 
 train_datagen = ImageDataGenerator(
@@ -70,9 +64,7 @@ test_generator = test_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary')
 
-# [8]
-
-# [9]
+# Build the CNN
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape, padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -91,33 +83,36 @@ model.add(Activation('sigmoid'))
 
 model.summary()
 
-# [10]
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-# [11]
 # Define the callbacks
-
 early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True)
 
-# [12]
-model.fit_generator(
+# Train model
+history = model.fit(
     train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
+    steps_per_epoch=nb_train_samples // batch_size // 6,
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size,
     callbacks=[early_stopping_cb])
 
-# [13]
 # Evaluate the model
-scores = model.evaluate_generator(test_generator)
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
+
+scores = model.evaluate(test_generator)
 
 print("Loss of the model: %.2f" % (scores[0]))
 print("Test Accuracy: %.2f%%" % (scores[1] * 100))
 
-# [14]
-# Saving the model for future use
+# Save the model
 
 model.save('model.h5')

ce50b289f743f616f2daa585b76c174f993fc577
Krystian Kosek <krystian.kosek17@gmail.com>
2021-05-29 14:24:16 +0200

    Super commit

---
+++ confusionMatrix.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sn
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+from tensorflow.python.keras.models import load_model
+
+main_dir = "./chest_xray3/"
+train_data_dir = main_dir + "train/"
+test_data_dir = main_dir + "test/"
+
+batch_size = 16
+img_height, img_width = 180, 180
+input_shape = (img_height, img_width, 3)
+
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    test_data_dir,
+    shuffle=False,
+    image_size=(img_height, img_width))
+
+model = load_model('preTrainedCovidModel.h5')
+class_names = test_ds.class_names
+
+y_pred = model.predict(test_ds)
+predicted_categories = tf.argmax(y_pred, axis=1)
+
+true_categories = tf.concat([y for x, y in test_ds], axis=0)
+
+cm = confusion_matrix(true_categories, predicted_categories)
+
+df_cm = pd.DataFrame(cm, index=[i for i in class_names],
+                     columns=[i for i in class_names])
+plt.figure(figsize=(10, 7))
+sn.heatmap(df_cm, annot=True, cmap='Blues', fmt='g')
+plt.ylabel("Prediction classes", fontsize=25)
+plt.xlabel("True classes", fontsize=25)
+plt.show()
--- testCovidModel.py
+++ testCovidModel.py
@@ -6,18 +6,14 @@ from keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
 import tensorflow as tf
 
-def predict_image(filename, img_height, img_width):
-    class_names = ['COVID', 'NORMAL', 'PNEUMONIA']
-
+def predict_image(class_names, model, filename, img_height, img_width):
     try:
         img = load_img(filename, target_size=(img_height, img_width))
     except:
         return 0
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create a batch
-    model = load_model('preTrainedCovidModel.h5')
     predictions = model.predict(img_array)
-    print(predictions)
     score = tf.nn.softmax(predictions[0])
 
     print(
@@ -25,6 +21,11 @@ def predict_image(filename, img_height, img_width):
             .format(class_names[np.argmax(score)], 100 * np.max(score))
     )
 
-for root, dirs, files in os.walk("./chest_xray3/test/COVID/"):
+
+class_names = ['COVID', 'NORMAL', 'PNEUMONIA']
+model = load_model('preTrainedCovidModel.h5')
+
+for root, dirs, files in os.walk("./chest_xray3/test/NORMAL/"):
     for filename in files:
-        predict_image("./chest_xray3/test/COVID/" + filename, 180, 180)
+        predict_image(class_names, model, "./chest_xray3/test/NORMAL/" + filename, 180, 180)
+
